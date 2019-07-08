81194fdfcdd6095937e965ddcba422a6cbe4a59d
Halis Yanmis <emreyanmis6161@gmail.com>
2019-07-08 12:02:26 -0400

    Add files via upload

---
+++ Plate.java
@@ -0,0 +1,19 @@
+package comp401sushi;
+
+import sushigame.model.Chef;
+
+public interface Plate { 
+	public enum Color {RED, GREEN, BLUE, GOLD};
+
+	Sushi getContents(); 
+	double getPrice(); 
+	Plate.Color getColor(); 
+
+	default double getProfit() {
+		return getPrice() - getContents().getCost();
+	}
+
+	Chef getChef();
+	//String getAgeOfPlateAtPosition(int position);     
+}
+
